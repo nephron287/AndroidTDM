ff6fdc84ab6242f8327b8046b9d58d3d9f95d28a
Nephron <nephron287@gmail.com>
2021-08-27 10:39:19 +1200

    Start project
    Start project

---
+++ New Unity Project/Assets/UI/Resources/Store/Scripts/UIGameOver.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIGameOver : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("escape"))  // если нажата клавиша Esc (Escape)
+        {
+            Application.Quit();    // закрыть приложение
+        }
+    }
+}
